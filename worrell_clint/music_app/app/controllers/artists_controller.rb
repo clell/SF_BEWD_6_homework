1c23421a2a008582a3f1ef9ccc1a20e8ec29e2e4
clell <github@clintworrell.com>
2014-10-21 13:43:26 -0700

    add edit and update actions

--- worrell_clint/music_app/app/controllers/artists_controller.rb
+++ worrell_clint/music_app/app/controllers/artists_controller.rb
@@ -17,6 +17,20 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+
+    if @artist.update(artist_params)
+      redirect_to artists_path
+    else
+      render 'edit'
+    end
+  end
+
   private
     def artist_params
       params.require(:artist).permit(:name)
