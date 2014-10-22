248b87f94b59263ef2dbee99d52f550f986991c5
clell <github@clintworrell.com>
2014-10-21 22:15:10 -0700

    add new and create actions. add song_params.

--- worrell_clint/music_app/app/controllers/songs_controller.rb
+++ worrell_clint/music_app/app/controllers/songs_controller.rb
@@ -3,4 +3,20 @@ class SongsController < ApplicationController
     @artist = Artist.find(params[:artist_id])
     @songs = @artist.songs.all
   end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @song = Song.new
+  end
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.create(song_params)
+    redirect_to artist_songs_path(@artist)
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:title)
+    end
 end
